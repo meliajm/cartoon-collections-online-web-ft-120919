62649c722eae57066d148619405cc5cc8d5e1ca5
melia miller <meliajm@gmail.com>
2019-12-11 01:45:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.detect{|element| i.include?(cheese_types)}
+  array.detect{|element| element.include?(cheese_types)}
 end
 
 # [1,2,3].detect{|i| i.odd?}
